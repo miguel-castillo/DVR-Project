a51f9c688074eed30385ec91f0768deff41b2a9c
mcmalfaro140 <mcmalfaro@hotmail.com>
2019-11-23 13:53:17 -0800

    error checking added

--- menu.py
+++ menu.py
@@ -7,6 +7,30 @@ arguments_size = len(sys.argv)
 #funtion to start the server
 def server():
     print("Server thread")
+
+#update function
+def update():
+    print("Update option")
+
+#step function
+def step():
+    print("Step option")
+
+#packets function
+def packets():
+    print("Packets option")
+
+#display function
+def display():
+    print("Display option")
+
+#disable function
+def disable():
+    print("Disable option")
+
+#crash function
+def crash():
+    print("Crash option")
     
 # main menu function
 def mainMenu():
@@ -14,23 +38,37 @@ def mainMenu():
     temp = ''
     while temp != 'exit' :
         command = input("Waiting for command: ")
-        if command == 'update':
-            print("Update option")
-        elif command == "step":
-            print("Step option")
-        elif command == "packets":
-            print("Packets option")
-        elif command == "display":
-            print("Display option")
-        elif command == "disable":
-            print("Disable option")
-        elif command == "crash":
-            print("Crash option")
-        elif command == "exit":
-            temp = 'exit'
-            print("Program terminated")
-        else:
-            print("Command not found")
+        splitCommand = command.split()
+        if len(splitCommand) > 0 :
+            if splitCommand[0] == 'update':
+                if len(splitCommand) > 3 :
+                    update()
+                else:
+                    print("Missing arguments")
+                    print("Use the following template:")
+                    print("update <server-ID1> <server-ID2> <Link Cost>")
+            elif splitCommand[0] == "step":
+                step()
+            elif splitCommand[0] == "packets":
+                packets()
+            elif splitCommand[0] == "display":
+                display()
+            elif splitCommand[0] == "disable":
+                if len(splitCommand) > 1 :
+                    if splitCommand[1].isnumeric() :
+                        disable()
+                    else:
+                        print("Enter a number for server ID")
+                else:
+                    print("Missing server ID")
+            elif splitCommand[0] == "crash":
+                crash()
+            elif splitCommand[0] == "exit":
+                temp = 'exit'
+                print("Program terminated")
+            else:
+                print("Command not found")
+
 
 #Main Program
 # It will check for input then it will begin the treads
