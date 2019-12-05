c9aa40a855b337046b9499d2cb85aa36208e0d4a
mcmalfaro140 <mcmalfaro@hotmail.com>
2019-12-04 19:44:19 -0800

    Added graph and display

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@ import argparse
 import sys, os
 import threading
 import socket
+import networkx as nx
 
 arguments_size = len(sys.argv)
 arguments = sys.argv
@@ -9,11 +10,13 @@ global my_port
 global my_server_port
 global my_server
 global servers
+global G
 
 #reading topology file
 def readFile(path):
     global servers
     servers = []
+    edges = []
     print("Reading file...")
     topologyStr = []
     with open(path) as my_file:
@@ -21,11 +24,49 @@ def readFile(path):
             temp = line.split()
             if len(temp) > 2 and  temp[1].find('.') !=-1:
                 servers.append(line)
-            topologyStr.append(line)
-    print("File read")
+            elif len(temp) > 2 and  temp[1].find('.') ==-1:
+                edges.append(line)
+            topologyStr.append(line) 
+           
     global my_server_port
     temp = topologyStr[2].split()
     my_server_port = int(temp[2])
+
+    #Add nodes and edges from file
+    global G
+    G = nx.Graph()
+    tempNode = []
+    nodes = topologyStr[1]
+    formated_edges = format_edges(edges)
+    G.add_nodes_from(nodes)
+    G.add_edges_from(formated_edges)
+    # Initialise routing table for every node
+    for node in G.nodes:
+        if node == '\n':
+            tempNode.append(node)
+        else:
+            G.nodes[node]['routing_table'] = RoutingTable(node)
+    #Removes extra empty nodes
+    for item in tempNode :
+        G.remove_node(item)
+    print("File read and Routing Table initialized...")
+
+
+# Formats edges according to the graph edge required form: a tuple with a weight i.e. (src, dest, {'weight':5})
+def format_edges(raw_edges):
+    edges = []
+    for edge in raw_edges:
+        edges.append((edge[0], edge[2], {'weight': float(edge[4])}))
+    return edges
+
+# Displays the routing table for a given node
+def print_routing_table(node):
+    routing_table = G.nodes[node]['routing_table'].routing_table
+    print("\n Routing table for %s:" % node)
+    for node in routing_table:
+        #Getting rid of extra space
+        if node != '\n':
+            print("\t", node, "\t", routing_table[node])
 
 #Error message for invalid server command
 def serverError():
@@ -69,8 +110,13 @@ def packets():
     print("Packets option")
 
 #display function
-def display():
-    print("Display option")
+def display(node):
+    routing_table = G.nodes[node]['routing_table'].routing_table
+    print("Routing table for %s:" % node)
+    for n in routing_table:
+        #Getting rid of extra space
+        if n != '\n':
+            print(node,"\t", n, "\t", routing_table[n])
 
 #disable function
 def disable():
@@ -100,7 +146,8 @@ def mainMenu():
             elif splitCommand[0] == "packets":
                 packets()
             elif splitCommand[0] == "display":
-                display()
+                for node in G.nodes:
+                    display(node)
             elif splitCommand[0] == "disable":
                 if len(splitCommand) > 1 :
                     if splitCommand[1].isnumeric() :
@@ -119,6 +166,17 @@ def mainMenu():
             else:
                 print("Command not found")
 
+class RoutingTable:
+
+    def __init__(self, node):
+        self.name = node
+        self.routing_table = {adj_node:G.adj[node][adj_node]['weight'] for adj_node in G.adj[node]} # add adjacent nodes to the routing table
+        non_adjacent_nodes = list(set(G.nodes) - set(G.adj[node]) - set(node)) 
+        self.routing_table.update({non_adj_node: float('inf') for non_adj_node in non_adjacent_nodes}) # add non-adjacent nodes to the routing table
+        self.routing_table[node] = 0
+    
+    def update_edge(self, dest, cost):
+        self.routing_table.update({dest: cost})
 
 #Main Program
 # It will check for input then it will begin the treads
